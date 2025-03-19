619f2ccca704054dca1820f20c5e926ac0eef834
DavidChauke <david.pixelshield@gmail.com>
2025-03-20 00:35:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ window.onload = () => {
     videoContainer.style.visibility = 'hidden';
     pdfContainer.style.opacity = '1';
     pdfContainer.style.visibility = 'visible';
-  }, 1000);  // 3000ms = 3 seconds
+  }, 2000);  // 3000ms = 3 seconds
 };
